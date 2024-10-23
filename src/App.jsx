7189105c7d9ce6149d76f71a58df956dab200e4a
shahryar firoozi <shahryarfiroozi1378@gmail.com>
2024-10-23 16:33:42 +0330

    I started the project and added several components & config & pages folders to it

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+
+function App() {
+  return (
+    <Routes>
+      <Route index />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
+  );
+}
+
+export default App;
