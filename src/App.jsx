9b2340c0efc36af01f7f4557edbe609fb687a230
shahryar firoozi <shahryarfiroozi1378@gmail.com>
2024-10-23 19:37:01 +0330

    Initial implementation of the project. Installing react-router-dom and routing the login and register pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
     <Routes>
-      <Route index />
+      <Route index element={<Navigate to="/login" replace />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/login" element={<LoginPage />} />
     </Routes>
--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -1,6 +1,17 @@
 
 export default function RegisterPage() {
   return (
-    <div>RegisterPage</div>
+    <div>
+      <div>
+        <span></span>
+        <h3>فرم ثبت نام</h3>
+      </div>
+      <div>
+        <input type="text" />
+        <input type="text" />
+        <input type="text" />
+        <button></button>
+      </div>
+    </div>
   )
 }
